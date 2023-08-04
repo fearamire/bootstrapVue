fff38e6b97957077db8335af01ab1b7cd88e13a7
fearamire <sambo.jones15@gmail.com>
2023-08-04 12:03:35 -0500

    ran build

---
+++ assets/Departments-2dc5bcd6.js
@@ -0,0 +1,1 @@
+import{_ as e,o as t,c}from"./index-360a5853.js";const n={};function r(a,o){return t(),c("h1",null,"Departments page")}const _=e(n,[["render",r]]);export{_ as default};
--- assets/Departments-7f1fca58.js
+++
@@ -1,1 +0,0 @@
-import{_ as e,o as t,c}from"./index-2cab59cf.js";const n={};function r(a,o){return t(),c("h1",null,"Departments page")}const _=e(n,[["render",r]]);export{_ as default};
